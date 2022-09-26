66f88763dda0f54c7e079346d77721a33a6faf4f
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-09-26 09:39:17 -0300

    Added the axios library to perform requests and stylized the presentation of api data to the user.

--- src/components/ListUsers.vue
+++ src/components/ListUsers.vue
@@ -1,29 +1,59 @@
 <template>
-  <div class="row mt-3">
-    <div class="col">
-      <ul class="list-group">
-        <li class="list-group-item">
-          {{users}}
-        </li>
-      </ul>
-    </div>
+  <div>
+    <ul class="list-group d-flex align-items-center justify-content-center">
+      <li
+        class="list-group-item d-flex align-items-center"
+        id="list"
+        v-for="user in users"
+        :key="user.id"
+      >
+        <span id="name">{{ user.name }}</span>
+        <span id="age">{{ user.age }} anos</span>
+        <span class="espacar"></span>
+        <button class="btn btn-primary btn-sm" title="Editar">
+          <i class="fa fa-pencil-alt"></i>
+        </button>
+        <button class="btn btn-danger btn-sm" title="Deletar">
+          <i class="fa fa-trash"></i>
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import config from "@/config/config";
+
 export default {
-    name: 'ListUsers',
-    data() {
-      return {
-        users: []
-      }
-    },
-    created() {
-      fetch('http://localhost:3000/users')
-      .then(response => response.json())
-      .then(response => {
-        this.users = response
-      })
-    }
-}
+  name: "ListUsers",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    axios.get(`${config.url}/users`).then((response) => {
+      this.users = response.data;
+    });
+  },
+};
 </script>
+
+<style scoped>
+#list {
+  width: 40%;
+}
+
+#name, #age {
+  width: 50%;
+}
+
+.espacar {
+  flex: 1 1 auto;
+}
+
+button {
+  margin-right: 8px;
+}
+</style>
